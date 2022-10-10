2e6408e204485308ab7310664ba98ceba762e372
Matthew Crossley <matthew.crossley@bp.com>
2022-10-10 14:49:42 +0100

    Add queen moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -6,6 +6,19 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let moves = []
+
+        const currentSquare = board.findPiece(this)
+
+        for (let i = -1; i <= 1; i++){
+            for (let j = -1; j <= 1; j++){
+                if (i === 0 && j === 0){
+                    continue
+                }
+                moves.push.apply(moves, this.checkAvailableMoves(board, currentSquare, i, j))
+            }
+        }
+
+        return moves;
     }
 }
