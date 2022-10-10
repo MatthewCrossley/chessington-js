5cdc135eefa32cbf24463eae9a482cf3c3388090
Matthew Crossley <matthew.crossley@bp.com>
2022-10-10 14:42:50 +0100

    Refactor rook move logic

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -7,41 +7,13 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        function checkRange(from, to, squareMaker) {
-            let moves = []
-            if (from < to){
-                var chk = (i) => i < to;
-                var inc = 1
-            } else {
-                var chk = i => i > to;
-                var inc = -1
-            }
-
-            for (let i = from; chk(i); i+=inc) {
-                let tmpMove = squareMaker(i)
-                if (board.availableSquare(tmpMove)) {
-                    moves.push(tmpMove)
-                } else {
-                    break
-                }
-            }
-            return moves
-        }
+        let moves = []
 
         const currentSquare = board.findPiece(this)
 
-        let vertSquareMaker = (i, c=currentSquare.col) => { return Square.at(i, c) }
-        let horiSquareMaker = (i, r=currentSquare.row) => { return Square.at(r, i) }
-        let moves = [].concat(
-            // check all squares above the corrent position
-            checkRange(currentSquare.row + 1, board.board.length, vertSquareMaker),
-            // check all squares below the current position
-            checkRange(currentSquare.row - 1, -1, vertSquareMaker),
-            // check all squares to the right
-            checkRange(currentSquare.col + 1, board.board.length, horiSquareMaker),
-            // check all squares to the left
-            checkRange(currentSquare.col - 1, -1, horiSquareMaker)
-        )
+        for (let inc of [[1,0], [-1,0], [0,1], [0,-1]]){
+            moves.push.apply(moves, this.checkAvailableMoves(board, currentSquare, ...inc))
+        }
 
         return moves;
     }
