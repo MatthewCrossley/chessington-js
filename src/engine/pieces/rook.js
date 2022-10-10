1e4602d261e62e6681eea935df5735661718bccd
Matthew Crossley <matthew.crossley@bp.com>
2022-10-10 13:51:16 +0100

    Add moves for rook

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class Rook extends Piece {
@@ -6,6 +7,43 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        function checkRange(from, to, squareMaker) {
+            let moves = []
+            if (from < to){
+                var chk = (i) => i < to;
+                var inc = 1
+            } else {
+                var chk = i => i > to;
+                var inc = -1
+            }
+
+            for (let i = from; chk(i); i+=inc) {
+                let tmpMove = squareMaker(i)
+                if (board.availableSquare(tmpMove)) {
+                    moves.push(tmpMove)
+                } else {
+                    break
+                }
+            }
+            return moves
+        }
+
+        const currentSquare = board.findPiece(this)
+
+        let moves = []
+
+        let vertSquareMaker = (i, c=currentSquare.col) => { return Square.at(i, c) }
+        // check all squares above the corrent position
+        moves = moves.concat(checkRange(currentSquare.row + 1, board.board.length, vertSquareMaker))
+        // check all squares below the current position
+        moves = moves.concat(checkRange(currentSquare.row - 1, -1, vertSquareMaker))
+
+        let horiSquareMaker = (i, r=currentSquare.row) => { return Square.at(r, i) }
+        // check all squares to the right
+        moves = moves.concat(checkRange(currentSquare.col + 1, board.board.length, horiSquareMaker))
+        // check all squares to the left
+        moves = moves.concat(checkRange(currentSquare.col - 1, -1, horiSquareMaker))
+
+        return moves;
     }
 }
