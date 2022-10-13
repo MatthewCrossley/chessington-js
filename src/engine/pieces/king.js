a5a1f9971413b8be9a7c82cb4db85780216d9e7f
Matthew Crossley <matthew.crossley@bp.com>
2022-10-13 16:24:55 +0100

    Add castling.

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,5 +1,7 @@
 import Piece from './piece';
 import Square from '../square'
+import Player from '../player';
+import Rook from './rook';
 
 export default class King extends Piece {
     constructor(player) {
@@ -7,11 +9,81 @@ export default class King extends Piece {
         this.cannotTake = true
     }
 
-    getAvailableMoves(board) {
+    squareisChecked(board, square){
+        let checkable = false
+        for (let row = 0; row < board.board.length; row++) {
+            for (let col = 0; col < board.board[row].length; col++) {
+                if (board.board[row][col] instanceof Piece) {
+                    let p = board.board[row][col]
+                    if (p.player === this.player){
+                        continue
+                    }
+
+                    if (p.canCheck(board, square)){
+                        checkable = true
+                        break
+                    }
+                }
+            }
+            if (checkable){
+                break
+            }
+        }
+        return checkable
+    }
+
+    castling(board){
+        let castleMoves = []
+        if (this.moveHistory.length !== 0){
+            return castleMoves
+        }
+        let currentSquare = board.findPiece(this)
+        if (this.squareisChecked(board, currentSquare)){
+            return castleMoves
+        }
+
+        let baseRow = this.player === Player.WHITE ? 0 : 7
+        let rooks = [
+            [board.getPiece(Square.at(baseRow, 0)), Square.at(baseRow, 0)],
+            [board.getPiece(Square.at(baseRow, 7)), Square.at(baseRow, 7)]
+        ]
+
+        for (let r of rooks){
+            let rook = r[0]
+            let rookPos = r[1]
+            if (rook === undefined || !rook instanceof Rook){
+                continue
+            }
+            if (rook.player !== this.player){
+                continue
+            }
+            if (rook.moveHistory.length !== 0){
+                continue
+            }
+            let direction = rookPos.col > currentSquare.col ? 1 : -1
+            let availableMoves = this.checkAvailableMoves(board, currentSquare, 0, direction)
+
+            if (availableMoves.length >= 2){
+                if (Math.abs(availableMoves.at(-1).col - rookPos.col) === 1){
+                    // if all squares are free up until the rook itself
+                    if (availableMoves.length === 2){
+                        castleMoves.push(availableMoves.at(-1))
+                    } else {
+                        // queenside rooks are 1 square further away
+                        castleMoves.push(availableMoves.at(-2))
+                    }
+                }
+            }
+        }
+        return castleMoves
+    }
+
+    _getAvailableMoves(board) {
         let moves = []
 
         let currentSquare = board.findPiece(this)
 
+        // check regular moves
         for (let i = -1; i <= 1; i++){
             for (let j = -1; j <= 1; j++){
                 if (i === 0 && j === 0){
@@ -21,33 +93,49 @@ export default class King extends Piece {
                 if (this.canMoveTo(board, tmpMove)){
                     // can we take the piece?
                     // Only if it cannot take the space we're trying to move to
-                    let checkable = false
-                    for (let row = 0; row < board.board.length; row++) {
-                        for (let col = 0; col < board.board[row].length; col++) {
-                            if (board.board[row][col] instanceof Piece) {
-                                let p = board.board[row][col]
-                                if (p.player === this.player){
-                                    continue
-                                }
-
-                                if (p.canCheck(board, tmpMove)){
-                                    checkable = true
-                                    break
-                                }
-                            }
-                        }
-                        if (checkable){
-                            break
-                        }
-                    }
-                    if (!checkable){
+                    if (!this.squareisChecked(board, tmpMove)){
                         moves.push(tmpMove)
                     }
                 }
             }
         }
 
+        // check castling
+        moves.push.apply(moves, this.castling(board))
+
         return moves;
+    }
+
+    moveTo(board, newSquare){
+        let castling = this.castling(board)
+        let kingSrc = board.findPiece(this)
+        for (let kingDest of castling){
+            let rookSrc = Square.at(kingSrc.row, kingDest.col > kingSrc.col ? 7 : 0)
+            if (newSquare.equals(kingDest)){
+                // if they have tried to select a castling square
+                let rook = board.getPiece(rookSrc)
+                let direction = kingDest.col > kingSrc.col ? 1 : -1
+                let rookDest = Square.at(kingSrc.row, kingSrc.col + direction)
+
+                board.setPiece(rookDest, rook)
+                board.setPiece(rookSrc, undefined)
+                super.moveTo(board, kingDest)
+                board.setPiece(rookSrc, undefined)
+                return
+            }
+        }
+        super.moveTo(board, newSquare)
+    }
+
+    checkAvailableMoves(board, currentSquare, rowIncrement, colIncrement){
+        let moves = []
+        for (let move of super.checkAvailableMoves(board, currentSquare, rowIncrement, colIncrement)){
+            if (this.squareisChecked(board, move)){
+                break
+            }
+            moves.push(move)
+        }
+        return moves
     }
 
     canCheck(board, square){
