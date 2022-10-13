7b88ac041253a266948d95aaf8d33c9cb4e6020f
Matthew Crossley <matthew.crossley@bp.com>
2022-10-13 10:35:39 +0100

    Fix kings moving into check by other kings

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -26,8 +26,7 @@ export default class King extends Piece {
                         for (let col = 0; col < board.board[row].length; col++) {
                             if (board.board[row][col] instanceof Piece) {
                                 let p = board.board[row][col]
-                                if (p instanceof King){
-                                    // tmp fix for recursion error
+                                if (p.player === this.player){
                                     continue
                                 }
 
@@ -49,5 +48,23 @@ export default class King extends Piece {
         }
 
         return moves;
+    }
+
+    canCheck(board, square){
+        let p = board.getPiece(square)
+        if (p !== undefined && p.player === this.player){
+            return false
+        }
+
+        let currentSquare = board.findPiece(this)
+        if (Math.abs(square.row - currentSquare.row) <= 1){
+            if (Math.abs(square.col - currentSquare.col) <= 1){
+                // king can only move 1 sqaure any direction
+                // so if piece is than 1 square away in any direction,
+                // we can check it
+                return true
+            }
+        }
+        return false
     }
 }
