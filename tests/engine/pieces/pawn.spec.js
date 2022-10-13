26e0a6b3bf74adf0730235877c28bf64c69ca9e2
Matthew Crossley <matthew.crossley@bp.com>
2022-10-13 13:35:20 +0100

    Add tests for en passant

--- tests/engine/pieces/pawn.spec.js
+++ tests/engine/pieces/pawn.spec.js
@@ -85,6 +85,44 @@ describe('Pawn', () => {
             moves.should.not.deep.include(Square.at(5, 3));
         });
 
+        it('en passant detected', () => {
+            const pawn = new Pawn(Player.WHITE);
+            const hostilePawn = new Pawn(Player.BLACK);
+
+            board.setPiece(Square.at(3, 3), pawn);
+            board.setPiece(Square.at(6, 4), hostilePawn);
+            
+            pawn.moveTo(board, Square.at(4, 3))  // white has to move first
+            hostilePawn.moveTo(board, Square.at(4, 4));
+    
+            const moves = pawn.getAvailableMoves(board);
+            moves.should.deep.include(Square.at(5, 4));
+        });
+
+        it('cannot take opposing non-pawns en passant', () => {
+            const pawn = new Pawn(Player.WHITE);
+            const hostilePiece = new Rook(Player.BLACK);
+            
+            board.setPiece(Square.at(3, 3), pawn);
+            board.setPiece(Square.at(6, 4), hostilePiece);
+            
+            pawn.moveTo(board, Square.at(4, 3))  // white has to move first
+            hostilePiece.moveTo(board, Square.at(4, 4));
+            
+            const moves = pawn.getAvailableMoves(board);
+            moves.should.not.deep.include(Square.at(5, 4));
+        });
+
+        it('cannot take opposing pawns en passant when not allowed', () => {
+            const pawn = new Pawn(Player.WHITE);
+            const hostilePawn = new Pawn(Player.BLACK);
+            
+            board.setPiece(Square.at(4, 3), pawn);
+            board.setPiece(Square.at(4, 4), hostilePawn);
+
+            const moves = pawn.getAvailableMoves(board);
+            moves.should.not.deep.include(Square.at(5, 4));
+        });
     });
 
     describe('black pawns', () => {
@@ -162,6 +200,45 @@ describe('Pawn', () => {
             const moves = pawn.getAvailableMoves(board);
 
             moves.should.not.deep.include(Square.at(3, 3));
+        });
+
+        it('en passant detected', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const hostilePawn = new Pawn(Player.WHITE);
+
+            board.setPiece(Square.at(4, 3), pawn);
+            board.setPiece(Square.at(1, 4), hostilePawn);
+            
+            pawn.moveTo(board, Square.at(3, 3))  // black has to move first
+            hostilePawn.moveTo(board, Square.at(3, 4));
+    
+            const moves = pawn.getAvailableMoves(board);
+            moves.should.deep.include(Square.at(2, 4));
+        });
+
+        it('cannot take opposing non-pawns en passant', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const hostilePiece = new Rook(Player.WHITE);
+            
+            board.setPiece(Square.at(6, 3), pawn);
+            board.setPiece(Square.at(3, 4), hostilePiece);
+            
+            pawn.moveTo(board, Square.at(5, 3))  // black has to move first
+            hostilePiece.moveTo(board, Square.at(5, 4));
+            
+            const moves = pawn.getAvailableMoves(board);
+            moves.should.not.deep.include(Square.at(4, 4));
+        });
+
+        it('cannot take opposing pawns en passant when not allowed', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const hostilePawn = new Pawn(Player.WHITE);
+            
+            board.setPiece(Square.at(4, 3), pawn);
+            board.setPiece(Square.at(4, 4), hostilePawn);
+
+            const moves = pawn.getAvailableMoves(board);
+            moves.should.not.deep.include(Square.at(3, 4));
         });
     });
 
